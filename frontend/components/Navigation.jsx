b771558bbe0ab2538b7b54434d303f8e735ede32
programsuyeon <programsuyeon@gmail.com>
2024-02-26 15:52:28 +0900

    initial upload_suyeon

--- frontend/components/Navigation.jsx
+++ frontend/components/Navigation.jsx
@@ -5,28 +5,32 @@ export default function Navigation() {
         <div className="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto bg-gray-900 lg:translate-x-0 lg:static lg:inset-0">
             <div className="flex items-center justify-center mt-7">
                 <div className="flex items-center">
+                <img alt="" src="/yong_icon.png" width={"60px"} height={"45px"}></img>
                     <Link href="/">
-                        <span className="mx-2 text-xl font-semibold text-white">Python FM Playground</span>
+                        <span className="mx-2 text-xl font-semibold text-white">로그인</span>
+                        {/* <button class="login-btn" id="loginBtn" onclick="location.href='/login_pop.html'">
+                        로그인
+                        </button> */}
                     </Link>
                 </div>
             </div>
             <nav className="mt-10">
-                <Link href="/models"
+                {/* <Link href="/models"
                       className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
                     <span className="mx-3">Foundation Models</span>
-                </Link>
+                </Link> */}
                 <Link href="/chat"
                       className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                    <span className="mx-3">Chat Playground</span>
+                    <span className="mx-3">Chitchatbot</span>
                 </Link>
-                <Link href="/text"
+                {/* <Link href="/text"
                       className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
                     <span className="mx-3">Text Playground</span>
                 </Link>
                 <Link href="/image"
                       className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
                     <span className="mx-3">Image Playground</span>
-                </Link>
+                </Link> */}
             </nav>
         </div>
     );
--- frontend/components/chatPlayground/ChatComponent.jsx
+++ frontend/components/chatPlayground/ChatComponent.jsx
@@ -63,11 +63,23 @@ export default function ChatContainer() {
     };
 
     return <div className="flex flex-col flex-auto h-full p-6">
-        <h3 className="text-3xl font-medium text-gray-700">Chat Playground</h3>
+        <h3 className="text-3xl font-medium text-gray-700">Chitchatbot</h3>
         <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 p-4 mt-8">
             <ModelIndicator modelName="Anthropic Claude 2" />
             <div className="flex flex-col h-full overflow-x-auto mb-4">
                 <div className="flex flex-col h-full">
+                    <div className="col-start-1 col-end-11 p-3 rounded-lg">
+                        <div className="flex flex-row items-center">
+                            <div
+                                className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                            >
+                            Bot
+                            </div>
+                            <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl rounded-br-xl">
+                            안녕하세요. ChitChatBot입니다. 처음 서비스를 이용하신다면 다음 질문들을 눌러 사용법을 알아보세요
+                            </div>
+                        </div>
+                    </div>
                     <div className="grid grid-cols-12 gap-y-2">
                         {conversation.map((item, i) => item.sender === "Assistant" ? (
                                 <Assistant text={item.message} key={i}/>
