0ab78552d29a52126cc5facf79e3b8a7230c9f73
hrk <39041842+sealotus1996@users.noreply.github.com>
2024-03-07 17:02:33 +0900

    Update Navigation.jsx
    add login button
    remove unnecessary parts

--- frontend/components/Navigation.jsx
+++ frontend/components/Navigation.jsx
@@ -3,35 +3,22 @@ export default function Navigation() {
 
     return (
         <div className="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto bg-gray-900 lg:translate-x-0 lg:static lg:inset-0">
-            <div className="flex items-center justify-center mt-7">
-                <div className="flex items-center">
-                <img alt="" src="/yong_icon.png" width={"60px"} height={"45px"}></img>
-                    <Link href="/">
-                        <span className="mx-2 text-xl font-semibold text-white">로그인</span>
-                        {/* <button class="login-btn" id="loginBtn" onclick="location.href='/login_pop.html'">
-                        로그인
-                        </button> */}
-                    </Link>
-                </div>
+
+            {/* 로그인 버튼 부분 */}
+            <div className="flex flex-row items-center space-between mt-7">
+                <img className="mx-2" alt="" src="/yong_icon.png" width={"60px"} height={"45px"}></img>
+                <span className="text-orange" id="idToInsert" style={{ width: "90px" }}></span>
+                <button className="mx-2 font-semibold text-white border border-white p-2" id="loginBtn" onclick="openLoginPopup()">
+                    로그인
+                </button>
             </div>
             <nav className="mt-10">
-                {/* <Link href="/models"
-                      className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                    <span className="mx-3">Foundation Models</span>
-                </Link> */}
                 <Link href="/"
-                      className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                    <span className="mx-3">Start New Chat</span>
+                    className="flex items-center px-3 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
+                    <span className="mx-1">Start New Chat</span>
                 </Link>
-                {/* <Link href="/text"
-                      className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                    <span className="mx-3">Text Playground</span>
-                </Link>
-                <Link href="/image"
-                      className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                    <span className="mx-3">Image Playground</span>
-                </Link> */}
             </nav>
         </div>
     );
 }
+
